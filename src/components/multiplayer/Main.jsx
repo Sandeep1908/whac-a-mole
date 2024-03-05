57057a2e0b41cf7ed3516d9a6cbf38b5409c93fc
Sandeep <sandbro7163337@gmail.com>
2024-03-05 13:45:18 +0530

    manage all player and score state

--- src/components/multiplayer/Main.jsx
+++ src/components/multiplayer/Main.jsx
@@ -1,30 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useId, useState } from "react";
 import getRandomNumber from "../../utils/getRandomNumber";
+
+import { Bounce, ToastContainer } from "react-toastify";
+import { useLocation } from "react-router-dom";
 import socket from "../../socket.io/socket";
-import { Bounce, ToastContainer, toast } from "react-toastify";
-import whac from "../../assets/images/welcome/mole.png";
+import { getUserId } from "../../utils/getUserId";
+
 function Main() {
   const [currentWhac, setCurrentWhac] = useState();
   const [intervalId, setIntervalId] = useState(null);
-  const [username,setUsername]=useState('')
- const [updatedScore,setUpdatedScore]=useState(0)
- const [users,setUser]=useState([])
+  const [roomUrl, setRoomUrl] = useState("");
+  const [roomJoined, setRoomJoined] = useState(false);
+  const location = useLocation();
+  const [opponent, setOpponent] = useState("");
+ 
+  const [localScore, setLocalScore] = useState(0);
+  const [opponentScore, setOpponentScore] = useState(0);
+  const [waiting,setIsWaiting]=useState(null)
 
   useEffect(() => {
-      socket.on('userjoined',(data)=>{
-   
-        setUser(data)
-        toast(`user ${data.currentUser} opponetUser: ${data.opponentUser} joined  ${data.room}`)
-      })
+    const host = window.location.host;
+    const path = location.pathname;
+    setRoomUrl(host + path);
   }, []);
+
+  useEffect(() => {
+    console.log("opponent",opponent)
+    socket.on("room-info", (data) => {
+      const players = Object.values(data.rooms.players);
+      
+      if(players.length>=2){
+        const opponent = players.find(
+          (player) => player.id !== localStorage.getItem("userId")
+        );
+          
+         
+        if (opponent) {
+          setOpponent(opponent?.id);
+          setRoomJoined(true);
+          setIsWaiting(false)
+        
+        }
+      }
+      
+    });
+
+
+    socket.on("opponentScoreUpdate", (score) => {
+      setOpponentScore(score);
+    });
+  }, [opponent]);
+
+  const handleJoinRoom = () => {
+    const userId = localStorage.getItem("userId");
+    const roomId = location.pathname;
+    socket.emit("join-game", { userId, roomId });
+    setIsWaiting(true)
+  };
 
   const handleStart = () => {
     const id = setInterval(() => {
       const randNo = getRandomNumber();
-
       setCurrentWhac(randNo);
     }, 1000);
-
     setIntervalId(id);
   };
 
@@ -38,8 +76,12 @@ function Main() {
   };
 
   const handleWhacClicked = (idx) => {
-    if (idx == currentWhac) {
-       console.log('hitted')
+    if (idx === currentWhac) {
+      setLocalScore(localScore + 1);
+      // Emit score update to the server
+      const userId = localStorage.getItem("userId");
+      const roomId = location.pathname;
+      socket.emit("scoreUpdate", roomId, userId, localScore + 1);
     } else {
       console.log("wrong");
     }
@@ -58,7 +100,7 @@ function Main() {
         draggable
         pauseOnHover
         theme="light"
-        transition:Bounce
+        transition={Bounce}
       />
 
       <div className="w-full h-full flex justify-center items-center">
@@ -67,51 +109,59 @@ function Main() {
             <h1 className="text-2xl text-center uppercase font-bold">
               Whac a mole
             </h1>
-          </div>
+            <p>Room name: {roomUrl}</p>
+           
+            {opponent &&   <p>Opponent Id:{opponent} opponent score:{opponentScore} your score:{localScore} </p>}
 
-          <div>
-            you:{users.currentUser} opponent:{users.opponentUser} room:{users.room}
+            {waiting && <p>Waiting for oppnent...</p>}
+          
           </div>
+          <div></div>
 
-          <div className="w-full h-full bg-green-500 grid grid-cols-3 md:grid-cols-4">
-            {[...Array(12).keys()].map((idx) => {
-              return (
-                <div
-                  key={idx}
-                  className={`w-32 h-32 border overflow-hidden  `}
-                  onClick={() => handleWhacClicked(idx)}
-                >
-                  <img
-                    src={whac}
-                    className={` object-contain  ${
-                      currentWhac === idx
-                        ? "whac-game-animation block"
-                        : "hidden"
-                    }`}
-                  />
-                </div>
-              );
-            })}
-          </div>
+          {roomJoined ? (
+            <div className="w-full h-full bg-green-500 grid grid-cols-3 md:grid-cols-4">
+              {[...Array(12).keys()].map((idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className={`w-32 h-32 border  ${
+                      currentWhac === idx ? "bg-red-500" : ""
+                    } `}
+                    onClick={() => handleWhacClicked(idx)}
+                  ></div>
+                );
+              })}
+            </div>
+          ) : (
+            <button
+              className="w-20 h-10 bg-blue-500 text-white rounded-md mt-4"
+              onClick={handleJoinRoom}
+            >
+              Join Room
+            </button>
+          )}
 
           <div className="w-full flex justify-center items-center gap-5 pt-10">
             <button
               className="w-20 h-10 bg-blue-500 text-white rounded-md"
-              onClick={() => handleStart()}
+              onClick={handleStart}
+              disabled={!roomJoined}
             >
               Play
             </button>
 
             <button
               className="w-20 h-10 bg-red-500 text-white rounded-md"
-              onClick={() => handleStop()}
+              onClick={handleStop}
+              disabled={!roomJoined}
             >
               Pause
             </button>
 
             <button
               className="w-20 h-10 bg-green-500 text-white rounded-md"
-              onClick={() => handleReset()}
+              onClick={handleReset}
+              disabled={!roomJoined}
             >
               Reset
             </button>
